b7a9e0d8ed0b6fee3d27bf2711b603ca990ae0a4
Robert Sturla <robertsturla@outlook.com>
2023-11-26 15:31:11 +0000

    Don't depend on environment variables during postinstall

--- nvidia/scripts/nvidia-postinstall.sh
+++ nvidia/scripts/nvidia-postinstall.sh
@@ -2,13 +2,8 @@
 
 set -ouex pipefail
 
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{,non}free{,-updates}.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{,non}free{,-updates,-updates-testing}.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/nvidia-container-toolkit.repo
-
-# If ENABLE_RPMFUSION_TESTING is set to true, disable the RPMFusion testing repos after installing the packages
-if [[ "${ENABLE_RPMFUSION_TESTING}" == "true" ]]; then
-  sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
-fi
 
 semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
 
