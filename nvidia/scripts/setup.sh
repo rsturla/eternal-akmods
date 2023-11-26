9aa0c518618930512595e5247a0004eaa030b13a
Robert Sturla <robertsturla@outlook.com>
2023-11-26 12:59:29 +0000

    Enable 545 and small script refactor

--- nvidia/scripts/setup.sh
+++ nvidia/scripts/setup.sh
@@ -2,13 +2,6 @@
 
 set -oeux pipefail
 
-# Create array of RPMFusion repositories to enable - e.g. rpmfusion-free-release, rpmfusion-nonfree-release, rpmfusion-nonfree-updates-testing
-RPMFUSION_REPOSITORIES=(
-  free-release
-  nonfree-release
-  nonfree-updates-testing
-)
-
 # Disable repos that are not needed for the build to improve build times
 if [[ "${FEDORA_VERSION}" -lt 39 ]]; then
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-modular}.repo
@@ -16,19 +9,17 @@ else
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-archive}.repo
 fi
 
-# If RPMFUSION_REPOSITORIES contains free-release, enable the free RPMFusion repository
-if [[ "${RPMFUSION_REPOSITORIES[*]}" =~ "free-release" ]]; then
-  rpm-ostree install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-  rpm-ostree install rpmfusion-free-release --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch
-fi
+rpm-ostree install \
+  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
-# If RPMFUSION_REPOSITORIES contains nonfree-release, enable the nonfree RPMFusion repository
-if [[ "${RPMFUSION_REPOSITORIES[*]}" =~ "nonfree-release" ]]; then
-  rpm-ostree install "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-  rpm-ostree install rpmfusion-nonfree-release --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
-fi
+rpm-ostree install \
+  rpmfusion-free-release \
+  rpmfusion-nonfree-release \
+  --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch \
+  --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
 
-# If RPMFUSION_REPOSITORIES contains nonfree-updates-testing, enable the nonfree RPMFusion updates-testing repository
-if [[ "${RPMFUSION_REPOSITORIES[*]}" =~ "nonfree-updates-testing" ]]; then
+# If ENABLE_TESTING_REPOS is set to true, enable the testing repos
+if [[ "${ENABLE_TESTING_REPOS}" == "true" ]]; then
   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
 fi
