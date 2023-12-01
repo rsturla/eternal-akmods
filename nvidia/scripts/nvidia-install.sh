b7fabfb954a81f9fc6ab6c5609bcf8b57cd73002
Robert Sturla <robertsturla@outlook.com>
2023-12-01 20:53:00 +0000

    Rename variables and store rpmfusion repo information in akmods cache file

--- nvidia/scripts/nvidia-install.sh
+++ nvidia/scripts/nvidia-install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 set -ouex pipefail
-
-ENABLE_RPMFUSION_TESTING=false
+source /var/cache/akmods/nvidia-vars
 
 # Modularity repositories are not available on Fedora 39 and above, so don't try to disable them
 if [[ "${FEDORA_VERSION}" -lt 39 ]]; then
@@ -11,15 +10,13 @@ else
   sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-archive}.repo
 fi
 
-# If ENABLE_RPMFUSION_TESTING is set to true, enable the RPMFusion testing repos
-if [[ "${ENABLE_RPMFUSION_TESTING}" == "true" ]]; then
+# If RPMFUSION_TESTING_ENABLED is set to true, enable the RPMFusion testing repos
+if [[ "${RPMFUSION_TESTING_ENABLED}" == "true" ]]; then
   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
 fi
 
 install -D /tmp/nvidia-addons/rpmbuild/SOURCES/nvidia-container-toolkit.repo \
   /etc/yum.repos.d/nvidia-container-toolkit.repo
-
-source /var/cache/akmods/nvidia-vars
 
 rpm-ostree install \
   xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
--- nvidia/scripts/nvidia-setup.sh
+++ nvidia/scripts/nvidia-setup.sh
@@ -19,11 +19,7 @@ rpm-ostree install \
   --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch \
   --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
 
-# If ENABLE_RPMFUSION_TESTING is set to true, enable the RPMFusion testing repos
-if [[ "${ENABLE_RPMFUSION_TESTING}" == "true" ]]; then
+# If RPMFUSION_TESTING_ENABLED is set to true, enable the RPMFusion testing repos
+if [[ "${RPMFUSION_TESTING_ENABLED}" == "true" ]]; then
   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
-
-  # Replace ENABLE_RPMFUSION_TESTING=false with ENABLE_RPMFUSION_TESTING=true in the /tmp/scripts/nvidia-install.sh file so it can
-  # be used in the images that implement this
-  sed -i 's@ENABLE_RPMFUSION_TESTING=false@ENABLE_RPMFUSION_TESTING=true@g' /tmp/scripts/nvidia-install.sh
 fi
