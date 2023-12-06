ffc721353bcead74f07fc76ea83061115b301cb8
Robert Sturla <robertsturla@outlook.com>
2023-12-06 01:22:14 +0000

    Enable fedora-repos-archive repository

--- nvidia/scripts/nvidia-setup.sh
+++ nvidia/scripts/nvidia-setup.sh
@@ -11,7 +11,8 @@ fi
 
 rpm-ostree install \
   "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
-  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" \
+  fedora-repos-archive
 
 rpm-ostree install \
   rpmfusion-free-release \
