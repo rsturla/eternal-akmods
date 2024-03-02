9f6b9ee46c12e9cf7c37620c314f41aa5e8fd2b3
Robert Sturla <robertsturla@outlook.com>
2024-03-02 19:36:12 +0000

    Explicitly disable testing repos

--- nvidia/scripts/nvidia-setup.sh
+++ nvidia/scripts/nvidia-setup.sh
@@ -13,6 +13,8 @@ rpm-ostree install \
   "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
   "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
+
 rpm-ostree install \
   rpmfusion-free-release \
   rpmfusion-nonfree-release \
