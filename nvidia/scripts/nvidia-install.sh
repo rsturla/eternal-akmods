085333a459c01a987ae3dc0b775d786063d9ddb7
Robert Sturla <robertsturla@outlook.com>
2024-03-02 19:40:24 +0000

    Explicitly disable rpmfusion testing repos before install

--- nvidia/scripts/nvidia-install.sh
+++ nvidia/scripts/nvidia-install.sh
@@ -14,6 +14,7 @@ else
 fi
 
 # If RPMFUSION_TESTING_ENABLED is set to true, enable the RPMFusion testing repos
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
 if [[ "${RPMFUSION_TESTING_ENABLED}" == "true" ]]; then
   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
 fi
