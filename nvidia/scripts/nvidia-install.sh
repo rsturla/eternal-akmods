666a19bad523eb3c8b8ceff52dde211b4ca35a33
Robert Sturla <robertsturla@outlook.com>
2024-04-10 23:42:17 +0100

    Fix rpmfusion sed command

--- nvidia/scripts/nvidia-install.sh
+++ nvidia/scripts/nvidia-install.sh
@@ -13,7 +13,7 @@ else
   sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-archive}.repo
 fi
 
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-{,updates}.repo
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}{,-updates}.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
 if [[ "${RPMFUSION_TESTING_ENABLED}" == "true" ]]; then
   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
