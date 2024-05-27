a5f7cf3c948aa370bfae576a7127945f8883be41
Robert Sturla <robertsturla@outlook.com>
2024-05-27 20:53:53 +0100

    fix: enable toolkit repo before install (#80)
    Enable toolkit repo before install

--- nvidia/scripts/install/01-install.sh
+++ nvidia/scripts/install/01-install.sh
@@ -5,6 +5,9 @@ source /tmp/akmods/info/nvidia-vars
 
 rpm-ostree install /tmp/akmods/rpms/nvidia-addons-*.rpm
 
+# Enable nvidia-container-toolkit repo
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/nvidia-container-toolkit.repo
+
 # Install Nvidia drivers
 rpm-ostree install \
   xorg-x11-drv-nvidia-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
