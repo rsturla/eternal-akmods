974e41f9b5697bbef5f3dbdb0d44176ee7688f85
Robert Sturla <robertsturla@outlook.com>
2024-05-27 18:41:43 +0100

    fix: install addons before drivers (#77)
    Install addons before drivers

--- nvidia/scripts/nvidia-install.sh
+++ nvidia/scripts/nvidia-install.sh
@@ -3,31 +3,10 @@
 set -ouex pipefail
 source /tmp/akmods/info/nvidia-vars
 
-# Create a backup of current repos
-cp -a /etc/yum.repos.d /tmp/yum.repos.d
-
-# Install rpmfusion repos
-rpm-ostree install \
-  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
-  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-
-rpm-ostree install \
-  rpmfusion-free-release \
-  rpmfusion-nonfree-release \
-  --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch \
-  --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
-
-# Configure rpmfusion repos
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-archive}.repo
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}{,-updates}.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
-if [[ "${RPMFUSION_TESTING_ENABLED}" == "true" ]]; then
-  sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
-fi
+rpm-ostree install /tmp/akmods/rpms/nvidia-addons-*.rpm
 
 # Install Nvidia drivers
 rpm-ostree install \
   xorg-x11-drv-nvidia-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
   nvidia-container-toolkit nvidia-vaapi-driver \
-  /tmp/akmods/rpms/kmod-nvidia-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm \
-  /tmp/akmods/rpms/nvidia-addons-*.rpm
+  /tmp/akmods/rpms/kmod-nvidia-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm
