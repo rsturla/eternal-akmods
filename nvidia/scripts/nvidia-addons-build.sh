eaba0ecf6916f5b34fbbc78b32e44c1d9ad40065
Robert Sturla <robertsturla@outlook.com>
2024-05-27 18:03:40 +0100

    fix: enable toolkit repo (#75)
    Enable toolkit repo

--- nvidia/scripts/nvidia-addons-build.sh
+++ nvidia/scripts/nvidia-addons-build.sh
@@ -8,6 +8,7 @@ curl -Lo ${RPMBUILD_SOURCES_DIR}/nvidia-container-toolkit.repo https://nvidia.gi
 curl -Lo ${RPMBUILD_SOURCES_DIR}/nvidia-container.pp https://raw.githubusercontent.com/NVIDIA/dgx-selinux/master/bin/RHEL9/nvidia-container.pp
 
 sed -i "s@gpgcheck=0@gpgcheck=1@" ${RPMBUILD_SOURCES_DIR}/nvidia-container-toolkit.repo
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/nvidia-container-toolkit.repo
 
 install -D /etc/pki/akmods/certs/public_key.der ${RPMBUILD_SOURCES_DIR}/public_key.der
 
