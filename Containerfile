39beb364d495a1861b1fd3e1ac725738e263bc11
Robert Sturla <robertsturla@outlook.com>
2024-05-28 00:22:55 +0100

    Initial pass at refactoring

---
+++ Containerfile
@@ -0,0 +1,30 @@
+ARG FEDORA_VERSION=40
+ARG KMOD_NAME=nvidia
+ARG KMOD_VERSION=550
+
+FROM quay.io/fedora-ostree-desktops/base:${FEDORA_VERSION} AS builder
+
+ARG FEDORA_VERSION
+ARG KMOD_NAME
+ARG KMOD_VERSION
+
+COPY _certs /tmp/certs
+COPY _scripts /tmp/scripts
+COPY kmods/${KMOD_NAME}/scripts/build /tmp/scripts
+COPY kmods/${KMOD_NAME}/rpm-specs /tmp/rpm-specs
+COPY kmods/${KMOD_NAME}/files /tmp/files
+
+RUN chmod +x /tmp/scripts/*.sh && \
+    /tmp/scripts/setup.sh && \
+    /tmp/scripts/00-prebuild.sh && \
+    /tmp/scripts/01-build.sh && \
+    /tmp/scripts/final.sh
+
+RUN rpm -ql /rpms/*.rpm
+
+
+FROM scratch AS artifacts
+
+COPY kmods/${KMOD_NAME}/scripts/install /tmp/scripts
+COPY --from=builder /rpms /rpms
+COPY --from=builder /var/cache/akmods/nvidia-vars /info/nvidia-vars
---
+++ kmods/nvidia/scripts/build/00-prebuild.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+rpm-ostree install \
+  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
+rpm-ostree install \
+  rpmfusion-free-release \
+  rpmfusion-nonfree-release \
+  --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch \
+  --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
+
+# If RPMFUSION_TESTING_ENABLED is set to true, enable the RPMFusion testing repos
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
+# if [[ "${RPMFUSION_TESTING_ENABLED}" == "true" ]]; then
+#   echo "INFO: Enabling RPMFusion testing repositories"
+#   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
+# fi
