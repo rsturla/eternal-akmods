34ed14bff3acfae61f6689c8e1404d9f54699bc2
Robert Sturla <robertsturla@outlook.com>
2024-05-27 17:18:11 +0100

    Explicitly install rpmfusion if not exists

--- nvidia/scripts/nvidia-install.sh
+++ nvidia/scripts/nvidia-install.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
 set -ouex pipefail
-source /info/nvidia-vars
+source /tmp/akmods/info/nvidia-vars
 
 # Create a backup of current repos
 cp -a /etc/yum.repos.d /tmp/yum.repos.d
 
+# Install rpmfusion repos
+rpm-ostree install \
+  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
+rpm-ostree install \
+  rpmfusion-free-release \
+  rpmfusion-nonfree-release \
+  --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch \
+  --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
+
+# Configure rpmfusion repos
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-archive}.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}{,-updates}.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
@@ -13,11 +25,9 @@ if [[ "${RPMFUSION_TESTING_ENABLED}" == "true" ]]; then
   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
 fi
 
-install -D /tmp/nvidia-addons/rpmbuild/SOURCES/nvidia-container-toolkit.repo \
-  /etc/yum.repos.d/nvidia-container-toolkit.repo
-
+# Install Nvidia drivers
 rpm-ostree install \
   xorg-x11-drv-nvidia-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
   nvidia-container-toolkit nvidia-vaapi-driver \
-  /rpms/kmod-nvidia-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm \
-  /rpms/nvidia-addons-*.rpm
+  /tmp/akmods/rpms/kmod-nvidia-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm \
+  /tmp/akmods/rpms/nvidia-addons-*.rpm
