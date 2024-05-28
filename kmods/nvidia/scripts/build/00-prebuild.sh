8b95c7676a2891a7ec649fc46a60f806210ad00e
Robert Sturla <robertsturla@outlook.com>
2024-05-28 23:42:48 +0100

    Add support for testing repos

--- kmods/nvidia/scripts/build/00-prebuild.sh
+++ kmods/nvidia/scripts/build/00-prebuild.sh
@@ -12,9 +12,9 @@ rpm-ostree install \
   --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch \
   --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
 
-# If RPMFUSION_TESTING_ENABLED is set to true, enable the RPMFusion testing repos
+# If REPOSITORY_TYPE is set to true, enable the RPMFusion testing repos
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
-# if [[ "${RPMFUSION_TESTING_ENABLED}" == "true" ]]; then
-#   echo "INFO: Enabling RPMFusion testing repositories"
-#   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
-# fi
+if [[ "${REPOSITORY_TYPE}" == "testing" ]]; then
+  echo "INFO: Enabling RPMFusion testing repositories"
+  sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
+fi
