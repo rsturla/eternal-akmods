c46bb37e350c4cfd60091b416d3fc3788351d3fd
Robert Sturla <robertsturla@outlook.com>
2024-06-23 17:16:22 +0100

    Remove repos-archive and move rpmfusion back to nvidia script

--- kmods/nvidia/scripts/build/00-prebuild.sh
+++ kmods/nvidia/scripts/build/00-prebuild.sh
@@ -2,6 +2,16 @@
 
 set -oeux pipefail
 
+rpm-ostree install \
+  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
+rpm-ostree install \
+  rpmfusion-free-release \
+  rpmfusion-nonfree-release \
+  --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch \
+  --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
+
 # If REPOSITORY_TYPE is set to true, enable the RPMFusion testing repos
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
 if [[ "${REPOSITORY_TYPE}" == "testing" ]]; then
