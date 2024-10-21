f67c7fad99a2e767eb4a70c7d769e9e5f42cd1b0
Robert Sturla <robertsturla@outlook.com>
2024-10-21 23:30:35 +0100

    fix: path to RPMs

--- _scripts/final.sh
+++ _scripts/final.sh
@@ -9,7 +9,7 @@ for rpm in $(find /var/cache -name '*.rpm'); do
 done
 
 sed -i -e 's/args = \["rpmbuild", "-bb"\]/args = \["rpmbuild", "-bb", "--buildroot", "#{build_path}\/BUILD"\]/g' /usr/local/share/gems/gems/fpm-*/lib/fpm/package/rpm.rb;
-for rpm in $(find /var/cache/rpms/kmods -type f -name \*.rpm); do
+for rpm in $(find /rpms -type f -name \*.rpm); do
   basename="$(basename ${rpm})"
   name="${basename%%-6*}"
   fpm --verbose -s rpm -t rpm -p ${rpm} -f --name ${name} ${rpm};
