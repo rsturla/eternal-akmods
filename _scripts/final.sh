e313c3bbc6482f8b8793d9f9720e0c27d07be394
Robert Sturla <robertsturla@outlook.com>
2024-10-22 11:20:18 +0100

    fix: only rename RPM if contains kernel version

--- _scripts/final.sh
+++ _scripts/final.sh
@@ -8,11 +8,20 @@ for rpm in $(find /var/cache/rpms -name '*.rpm'); do
   cp -a $rpm /rpms
 done
 
+kernel_version=$(uname -r)
+
 sed -i -e 's/args = \["rpmbuild", "-bb"\]/args = \["rpmbuild", "-bb", "--buildroot", "#{build_path}\/BUILD"\]/g' /usr/local/share/gems/gems/fpm-*/lib/fpm/package/rpm.rb;
+
 for rpm in $(find /rpms -type f -name \*.rpm); do
   basename="$(basename ${rpm})"
   name="${basename%%-6*}"
-  fpm --verbose -s rpm -t rpm -p ${rpm} -f --name ${name} ${rpm};
+
+  if [[ "$basename" == *"$kernel_version"* ]]; then
+    echo "Processing $rpm with kernel version $kernel_version..."
+    fpm --verbose -s rpm -t rpm -p ${rpm} -f --name ${name} ${rpm};
+  else
+    echo "Skipping $rpm as it does not contain kernel version $kernel_version..."
+  fi
 done
 
 ls -l /rpms
