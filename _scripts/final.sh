7dcbf2465f0212ae482d44724fc54278a2014c01
Robert Sturla <robertsturla@outlook.com>
2024-10-22 11:37:02 +0100

    fix: only rename RPM if contains kernel version (#128)
    * fix: only rename RPM if contains kernel version
    
    * fix: support other kernel versions
    
    * chore: limit workflow concurrency
    
    * fix: get kernel version from RPM

--- _scripts/final.sh
+++ _scripts/final.sh
@@ -8,11 +8,21 @@ for rpm in $(find /var/cache/rpms -name '*.rpm'); do
   cp -a $rpm /rpms
 done
 
+kernel_version=$(rpm -q --qf "%{VERSION}-%{RELEASE}.%{ARCH}\n" kernel-core | head -n 1)
+
 sed -i -e 's/args = \["rpmbuild", "-bb"\]/args = \["rpmbuild", "-bb", "--buildroot", "#{build_path}\/BUILD"\]/g' /usr/local/share/gems/gems/fpm-*/lib/fpm/package/rpm.rb;
+
 for rpm in $(find /rpms -type f -name \*.rpm); do
   basename="$(basename ${rpm})"
-  name="${basename%%-6*}"
-  fpm --verbose -s rpm -t rpm -p ${rpm} -f --name ${name} ${rpm};
+  # Remove kernel version from the RPM name (e.g. kmod-nvidia-6.11.3-300.fc41.x86_64-3:560.35.03-2.fc41.x86_64 -> kmod-nvidia-3:560.35.03-2.fc41.x86_64)
+  name=$(echo ${basename} | sed -r "s/(-${kernel_version})//g")
+
+  if [[ "$basename" == *"$kernel_version"* ]]; then
+    echo "Processing $rpm with kernel version $kernel_version..."
+    fpm --verbose -s rpm -t rpm -p ${rpm} -f --name ${name} ${rpm};
+  else
+    echo "Skipping $rpm as it does not contain kernel version $kernel_version..."
+  fi
 done
 
 ls -l /rpms
